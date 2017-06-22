1a23ac6cc5c5d583e4e47fd4048cec59f4d879b1
Jonny Holupchinski <jonholup@gmail.com>
2017-06-22 16:26:00 -0500

    added feedback

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace FitnessFrog
             // Prompt user for minutes exercised 
             Console.Write("Enter how many minutes you exercised, or type \"quit\" to exit: ");
             string newEntry = Console.ReadLine();
-            
+
             // Repeat until user quits 
             if (newEntry == "quit")
             {
@@ -26,9 +26,31 @@ namespace FitnessFrog
               // Add minutes exercised to total 
               runningTotal = runningTotal + newInt;
       
-              
+              // Print feedback dependent on newInt
+              if (newInt <= 15)
+              {
+                Console.WriteLine("I like your style!");
+              }
+              else if (newInt <= 30)
+              {
+                Console.WriteLine("Way to go!");
+              }
+              else if (newInt <= 60)
+              {
+                Console.WriteLine("Ooof!");
+              }
+              else if (newInt >= 61)
+              {
+                Console.WriteLine("Insanity!");
+              }
+              else
+              {
+                Console.WriteLine("Does not compute");
+              }
+
               // Display total minutes exercised to the screen 
-              Console.WriteLine("You've entered " + runningTotal + " minutes");  
+              Console.WriteLine("You've entered " + runningTotal + " minutes");
+
             }
             
         } 
