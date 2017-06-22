aa8eec6f9d3e919bc2d1e7dad1a20e5d02238bd6
Jonny Holupchinski <jonholup@gmail.com>
2017-06-22 13:42:07 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace FitnessFrog
+{
+    class Program
+  {
+      static void Main()
+      {
+          // Prompt user for minutes exercised 
+          Console.Write("Enter how many minutes you exercised: ");
+          
+          string entry = Console.ReadLine();
+        
+  
+          // Add minutes exercised to total 
+          // Display total minutes exercised to the screen 
+          // Repeat until user quits    
+      }
+  }
+}
