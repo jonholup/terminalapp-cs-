20e1048ce15d7f2de5fd945f42d0c730dde1e866
Jonny Holupchinski <jonholup@gmail.com>
2017-06-22 15:52:52 -0500

    tracking minutes per session, user can logout

--- Program.cs
+++ Program.cs
@@ -7,16 +7,32 @@ namespace FitnessFrog
       static void Main()
       {
         int runningTotal = 0;
-          // Prompt user for minutes exercised 
-          Console.Write("Enter how many minutes you exercised: ");
-          int newEntry = int.Parse(Console.ReadLine());
-          // Add minutes exercised to total 
-          runningTotal = runningTotal + newEntry;
-  
-          
-          // Display total minutes exercised to the screen 
-          Console.WriteLine("You've entered " + runningTotal + " minutes");
-          // Repeat until user quits    
+        bool runFrog = true;
+
+        while (runFrog)
+        {
+            // Prompt user for minutes exercised 
+            Console.Write("Enter how many minutes you exercised, or type \"quit\" to exit: ");
+            string newEntry = Console.ReadLine();
+            
+            // Repeat until user quits 
+            if (newEntry == "quit")
+            {
+              runFrog = false;
+            } 
+            else
+            {
+              int newInt = int.Parse(newEntry);
+              // Add minutes exercised to total 
+              runningTotal = runningTotal + newInt;
+      
+              
+              // Display total minutes exercised to the screen 
+              Console.WriteLine("You've entered " + runningTotal + " minutes");  
+            }
+            
+        } 
+        Console.WriteLine("Goodbye");
       }
   }
 }
